e350eeb6ab6e72f95ff2405581be57c37d1c8151
dream_Faunus <cappu159@gmail.com>
2021-03-29 23:14:07 +0900

    Create infinite scroll-demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
-function App() {
+const PAGE_NUMBER = 4;
+
+export default function App() {
+  const [state, setState] = useState([]);
+  const [page, setPage] = useState(PAGE_NUMBER);
+
+  useEffect(() => {
+    fetch(`https://dog.ceo/api/breeds/image/random/${page}`)
+      .then(res => res.json())
+      .then(json => setState([...state, ...json.message]))
+  }, [page])
+
+  const scrollToEnd = () => {
+    setPage( page + 1 )
+  }
+
+  window.onscroll = function() {
+    //check if the page has scrolled to the bottom
+    if (
+      window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight
+    ) {
+      scrollToEnd()
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      {
+        state.length > 0 && state.map((elem, i)=>
+          <div key={i} className="container">
+            <img src={elem} alt=""/>            
+          </div>
+        )
+      }
+    </div>    
+  )
 }
 
-export default App;
+      {/* {state.length > 0 && state.map((el, i) => 
+        <div key={i} className={'container'}>
+          <h4>ID : {el._id}</h4>
+          <h4>Name : {el.name}</h4>
+          <h4>Trips: {el.trips}</h4>
+        </div>
+      )} */}
